81cafce74b91468e0172c089633a10a8f990d85f
admin <admin@example.com>
2023-03-09 07:32:31 +0000

    Day 1

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Navbar from "./components/Navbar.vue";
+import PeopleListing from "./components/People-Listing.vue"
+
+const peopleData: any = [
+    {
+      name : "abc"
+    },
+    {
+      name : "def"
+    },
+    {
+      name : "xyz"
+    }
+]
+
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+      <div class="row">
+        <div class="col-3">Hello world!!!</div>
+        <div class="col-9">
+          <PeopleListing v-bind:people="peopleData" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/People-Listing.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+
+export default {
+    props: {
+        people: {
+            type: Array
+        }
+    },
+    data: function () {
+        return {
+            message: "hello world!!!"
+        }
+    },
+    methods: {
+
+    }
+}
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="person in people">
+            {{
+                person.name
+            }}
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+* {
+    color: red
+}
+</style>
