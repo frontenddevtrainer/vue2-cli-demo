0f3134ef8677a43bf574cc1c3fcfd6fdded75cf6
admin <admin@example.com>
2023-03-10 05:57:45 +0000

    Added datatype for Data prop

--- src/components/People-Listing.vue
+++ src/components/People-Listing.vue
@@ -3,12 +3,13 @@
 // Axios - 3rd Party
 
 import Axios from "axios";
+import type { Person } from "../interfaces/person"
 
 export default {
     mounted() {
         this.loadData();
     },
-    data: function () {
+    data: function (): { companyName: string, time: Date, interval: number, people: Person[] } {
         return {
             companyName: "tavant",
             time: new Date(),
@@ -47,12 +48,12 @@ export default {
         <div>
             <p>Current time: {{ time }}</p>
             <p>Total records: {{ totalRecords }} in {{ companyNameFormatted }}</p>
-            <b-card v-for="person in people" :title="person.role" :img-src="person.profilePic" img-alt="Image" img-top
-                tag="article" style="max-width: 20rem;" class="mb-2">
+            <b-card v-for="person in people" :title="person.username" img-alt="Image" img-top tag="article"
+                style="max-width: 20rem;" class="mb-2">
                 <b-card-text>
-                    {{ person.name }} {{ person.age }}
+                    {{ person.name }} {{ person.phone }}
                 </b-card-text>
-                <b-button :href="person.profileLink" variant="primary">Go somewhere</b-button>
+                <b-button :href="person.website" variant="primary">Go somewhere</b-button>
             </b-card>
         </div>
     </div>
