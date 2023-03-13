7341ff0bda22e3602ccb574eafab6fbcf7c4930a
frontenddevtrainer <71391334+frontenddevtrainer@users.noreply.github.com>
2023-03-13 12:36:26 +0530

    Create products-detail.vue

---
+++ src/pages/products-detail.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export default {
+        props : ["id"],
+        computed: {
+            id() : any {
+                return this.$route.params.id + "computed"
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/products">Products</router-link>
+        <!-- <div><PeopleListing  /></div> -->
+        Detail Page
+        {{ id }}
+        
+     </div>
+</template>
