5bb7411bca2c592b0fdb7c3c2fbb9088f93e2b68
frontenddevtrainer <71391334+frontenddevtrainer@users.noreply.github.com>
2023-03-13 12:00:13 +0530

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,25 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Homepage from "../pages/homepage.vue";
+import Products from "../pages/products.vue";
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: "history",
   base: import.meta.env.BASE_URL,
   routes: [
+    {
+      path: "/",
+      component: Homepage
+    }, {
+      path: "/products",
+      component: Products
+    }
   ],
 });
 
 export default router;
+
+
